10fdc8345a1b1b580a40131ab363c2aa8f6714fb
donghyundonghyun <andy5153a@naver.com>
2017-01-18 13:57:00 +0900

    add nearby ories

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'cnddkf$04';
+$db['default']['password'] = '1q2w3e4r!';
 $db['default']['database'] = 'oricam';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
--- application/controllers/myserver.php
+++ application/controllers/myserver.php
@@ -15,5 +15,29 @@ class Myserver extends CI_Controller {
 		echo json_encode($this->oj_model->getInfo($ID));
 	}
 	
+	function nearbyOries($lat, $lon){
+		$ories = $this->oj_model->getOries();
+
+		$nearby = array();
+		$i = 0;
+		foreach($ories as $ori){
+			if($this->getDistance($lat, $lon, $ori->lat, $ori->lon)*100 < 20.0)
+				$nearby[$i++] = $ori;
+		}
+
+		echo json_encode($nearby);	
+		//var_dump($nearby);
+		
+	}
+
+	function getDistance($lat1, $lng1, $lat2, $lng2){
+	    $earth_radius = 6371;
+	    $dLat = deg2rad($lat2 - $lat1);
+	    $dLon = deg2rad($lng2 - $lng1);
+	    $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon/2) * sin($dLon/2);
+	    $c = 2 * asin(sqrt($a));
+	    $d = $earth_radius * $c;
+	    return $d;
+	}
 	
 }
